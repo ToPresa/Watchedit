26ca07ccc222da3a2efc63eb10e2b48aa7e5f5c9
ToPresa <topresa16@hotmail.com>
2016-11-21 18:50:53 +0100

    l

---
+++ app/src/main/java/com/watchedit/android/HotPage.java
@@ -0,0 +1,13 @@
+package com.watchedit.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HotPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_page);
+    }
+}

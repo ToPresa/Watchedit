ee390156db6a1cc897b45536878731bfc9dfcf95
ToPresa <topresa16@hotmail.com>
2016-12-08 21:29:27 +0100

    hots

--- app/src/main/java/com/watchedit/android/Display.java
+++ app/src/main/java/com/watchedit/android/Display.java
@@ -1,26 +1,37 @@
 package com.watchedit.android;
 
         import android.app.Activity;
+        import android.graphics.Bitmap;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.ArrayAdapter;
         import android.widget.ImageView;
+        import android.widget.RatingBar;
         import android.widget.TextView;
+
+        import com.squareup.picasso.Picasso;
+
+        import java.io.InputStream;
 
 public class Display extends ArrayAdapter<String> {
 
     private final Activity context;
     private final String[] itemname;
-    private final Integer[] imgid;
+    private final String[] imgid;
+    private final String[] rates;
+    private final String[] dates;
 
-    public Display(Activity context, String[] itemname, Integer[] imgid) {
+
+    public Display(Activity context, String[] itemname, String[] imgid, String[] rated, String[] dates) {
         super(context, R.layout.list, itemname);
         // TODO Auto-generated constructor stub
 
         this.context=context;
         this.itemname=itemname;
         this.imgid=imgid;
+        this.rates=rated;
+        this.dates=dates;
     }
 
     public View getView(int position,View view,ViewGroup parent) {
@@ -30,10 +41,12 @@ public class Display extends ArrayAdapter<String> {
         TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
         TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+        RatingBar ratess = (RatingBar) rowView.findViewById(R.id.ratingBar);
 
         txtTitle.setText(itemname[position]);
-        imageView.setImageResource(imgid[position]);
-        extratxt.setText("Description "+itemname[position]);
+        Picasso.with(context).load(imgid[position]).into(imageView);
+        extratxt.setText(dates[position]);
+        ratess.setRating(Float.parseFloat(rates[position])/2);
         return rowView;
 
     };
--- app/src/main/java/com/watchedit/android/HotShowFragment.java
+++ app/src/main/java/com/watchedit/android/HotShowFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,40 +14,23 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotShowFragment extends Fragment implements AsyncResponse {
 
     ListView list;
-    String[] itemname ={
-            "Safari",
-            "Camera",
-            "Global",
-            "FireFox",
-            "UC Browser",
-            "Android Folder",
-            "VLC Player",
-            "Cold War",
-            "Cold War",
-            "Cold War",
-            "Cold War",
-            "Cold War",
-            "Cold War"
-    };
-
-    Integer[] imgid={
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu,
-            R.drawable.menu
-    };
+    List<String> itemname = new ArrayList<String>();
+    List<String> itemimg = new ArrayList<String>();
+    List<String> itemrate = new ArrayList<String>();
+    List<String> itemdate = new ArrayList<String>();
+    String[] names;
+    String[] imgs;
+    String[] rates;
+    String[] dates;
     private String APIKEYthemovieDB = "cc0ee2bbfea45383a8c9381a4995aecd";
     public HotShowFragment() {
         // Required empty public constructor
@@ -62,38 +46,60 @@ public class HotShowFragment extends Fragment implements AsyncResponse {
 
         APIcall call = new APIcall();
         call.delegate=this;
-       // call.execute("https://api.themoviedb.org/3/tv/popular?api_key="+APIKEYthemovieDB+"&language=en-US");
+       call.execute("https://api.themoviedb.org/3/tv/popular?api_key="+APIKEYthemovieDB+"&language=en-US");
 
     }
-
-    @Override
-    public void processFinish(String asyncresult){
-        //This method will get call as soon as your AsyncTask is complete. asyncresult will be your result.
-        //TextView myAwesomeTextView = (TextView)findViewById(R.id.textView);
-        //myAwesomeTextView.setText(asyncresult);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_hot_show, container, false);
-                list=(ListView)view.findViewById(R.id.list);
-        Display adapter=new Display(this.getActivity(), itemname, imgid);
+
+        return view;
+    }
+    @Override
+    public void processFinish(String asyncresult){
+        //This method will get call as soon as your AsyncTask is complete. asyncresult will be your result.
+
+try {
+    JSONObject json = new JSONObject(asyncresult);
+    JSONArray a = json.getJSONArray("results");
+    for (int i = 0; i < a.length() && i<10; ++i) {
+        json = a.getJSONObject(i);
+        itemname.add((String) (json.getString("name")));
+        itemimg.add("https://image.tmdb.org/t/p/w500"+(json.getString("poster_path")));
+        itemrate.add((String) (json.getString("vote_average")));
+        itemdate.add("First aired: "+ (json.getString("first_air_date")));
+    }
+}catch(Exception e){
+    return;
+}
+
+        names = new String[ itemname.size() ];
+        itemname.toArray( names );
+        imgs = new String[ itemimg.size() ];
+        itemimg.toArray( imgs );
+        rates = new String[ itemrate.size() ];
+        itemrate.toArray( rates );
+        dates = new String[ itemdate.size() ];
+        itemdate.toArray( dates );
+
+        list=(ListView)getActivity().findViewById(R.id.list);
+        Display adapter=new Display(this.getActivity(), names, imgs, rates, dates);
 
         list.setAdapter(adapter);
-
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // TODO Auto-generated method stub
-                String Slecteditem= itemname[+position];
+                String Slecteditem= names[+position];
                 Toast.makeText(getActivity().getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
 
             }
         });
-        return view;
     }
+
+
 }
